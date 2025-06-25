45ba45458a8c177e1d637203cd8cd6bed8cb2f18
Juan Ignacio Canosa <juancanosa450@gmail.com>
2025-06-25 20:04:59 -0300

    Corrige click de botón ENTRAR con scroll y JS

--- buscar_turno.py
+++ buscar_turno.py
@@ -53,7 +53,11 @@ def buscar_turno():
 
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "vUSUARIO"))).send_keys(USUARIO_COSEM)
         driver.find_element(By.ID, "vPASSWORD").send_keys(PASSWORD_COSEM)
-        driver.find_element(By.ID, "ENTRAR").click()
+        btn_entrar = WebDriverWait(driver, 15).until(
+            EC.element_to_be_clickable((By.ID, "ENTRAR"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView(true);", btn_entrar)
+        driver.execute_script("arguments[0].click();", btn_entrar)
 
         WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//span[contains(text(), "Agenda")]'))).click()
         WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.ID, "NUEVOTURNO"))).click()
