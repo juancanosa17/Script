492a372851548bdd2e64c5e545dc2196bce6e977
Juan Ignacio Canosa <juancanosa450@gmail.com>
2025-06-25 20:14:52 -0300

    Actualización: ocultar datos sensibles, limpiar estructura

--- idchat.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-BOT_TOKEN = '7691304523:AAG61IIt4_JxJS5_-jt3wSZdA1hME3-Wg2E'  # Tu token
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/getUpdates"
-
-response = requests.get(url)
-data = response.json()
-
-print(data)
